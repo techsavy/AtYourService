f6c258db0432f3319d869aff449e1fbcc8b2ce15
techsavy <erangak@gmail.com>
2012-08-17 13:54:39 +0530

    Review creating and related tests

---
+++ Domain/Moderation/CreateReviewCommand.cs
@@ -0,0 +1,51 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CreateReviewCommand.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AtYourService.Domain.Moderation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Adverts;
+    using Core.Commanding;
+    using Users;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class CreateReviewCommand : EntityCommand
+    {
+        public CreateReviewCommand(int serviceId, int clientId, byte score, string body)
+        {
+            ServiceId = serviceId;
+            ClientId = clientId;
+            Score = score;
+            Body = body;
+        }
+
+        public int ServiceId { get; private set; }
+
+        public int ClientId { get; private set; }
+
+        public byte Score { get; private set; }
+
+        public string Body { get; private set; }
+
+        protected override void OnExecute()
+        {
+            var review = new Review
+                             {
+                                 Body = Body,
+                                 Score = Score,
+                                 Client = new Client { Id = ClientId },
+                                 Service = new ServiceOffering { Id = ServiceId }
+                             };
+            OnCreate(review);
+            Session.Save(review);
+        }
+    }
+}
---
+++ DomainTests/Moderation/CreateReviewTest.cs
@@ -0,0 +1,59 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CreateReviewTest.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AtYourService.Domain.Tests.Moderation
+{
+    using Categories;
+    using Domain.Adverts;
+    using Domain.Moderation;
+    using NUnit.Framework;
+    using Users;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class CreateReviewTest : NHibernateIntegrationTestBase
+    {
+        [Test]
+        public void Create_Review_With_All_Required_Information()
+        {
+            int serviceId, clientId;
+            PupulateEntities(out clientId, out serviceId);
+
+            const string body = "Review Body";
+            var command = new CreateReviewCommand(serviceId, clientId, 5, body);
+            ExecuteCommand(command);
+
+            var review = Session.QueryOver<Review>().Where(r => r.Service.Id == serviceId && r.Client.Id == clientId)
+                .And(r => r.Body == body).SingleOrDefault();
+
+            Assert.NotNull(review);
+            Assert.AreEqual(body, review.Body);
+        }
+
+        void PupulateEntities(out int clientId, out int serviceId)
+        {
+            var createCategory = new AddCategoryCommand(null, "test");
+            ExecuteCommand(createCategory);
+
+            var createClientCommand = new CreateClientCommand("test", "test@example.com", "123456", "brag", 23.565, 12.34, 1);
+            ExecuteCommand(createClientCommand);
+
+            var category = Session.QueryOver<Category>().FutureValue();
+            var client = Session.QueryOver<Client>().FutureValue();
+
+            var categoryId = category.Value.Id;
+            clientId = client.Value.Id;
+
+            var createServiceCommand = new CreateServiceCommand(true, "title", "body", categoryId, clientId, 23.234, 5.343, null, null, null);
+            ExecuteCommand(createServiceCommand);
+
+            var service =  Session.QueryOver<Service>().SingleOrDefault();
+
+            serviceId = service.Id;
+        }
+    }
+}
