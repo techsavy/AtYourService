3cd7568b04ddc89c866b359988dd3f0b742ce69e
techsavy <erangak@gmail.com>
2012-09-24 17:05:07 +0530

    report enhansments

--- AtYourServiceWeb/Util/DateTimeExtensions.cs
+++ AtYourServiceWeb/Util/DateTimeExtensions.cs
@@ -3,6 +3,8 @@
 namespace AtYourService.Web.Util
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public static class DateTimeExtensions
     {
@@ -13,6 +15,22 @@ namespace AtYourService.Web.Util
             var milliseconds = unixTicks / 10000;
 
             return milliseconds / (60 * 1000);
+        }
+
+        public static DateTime MinDate(this IEnumerable<DateTime> left, IEnumerable<DateTime> right)
+        {
+            var minLeft = left.Any() ? left.Min() : DateTime.MaxValue;
+            var minRight = right.Any() ? right.Min() : DateTime.MaxValue;
+
+            return minLeft.Ticks < minRight.Ticks ? minLeft : minRight;
+        }
+
+        public static DateTime MaxDate(this IEnumerable<DateTime> left, IEnumerable<DateTime> right)
+        {
+            var maxLeft = left.Any() ? left.Max() : DateTime.MinValue;
+            var maxRight = right.Any() ? right.Max() : DateTime.MinValue;
+
+            return maxLeft.Ticks > maxRight.Ticks ? maxLeft : maxRight;
         }
     }
 }
