78f2dff06875f8fcac2db8801970d7a86719276f
techsavy <erangak@gmail.com>
2012-11-13 15:18:57 +0530

    Flagging services

---
+++ AtYourServiceWeb/Controllers/FlagsController.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace AtYourService.Web.Controllers
+{
+    using System.Web.Mvc;
+    using Domain.Moderation;
+    using Models;
+    using Util;
+
+    public class FlagsController : BaseController
+    {
+        public FlagsController(NHibernateContext nHibernateContext) : base(nHibernateContext)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public JsonResult Create(FlagModel flagModel)
+        {
+            ExecuteCommand(new FlagServiceCommand(UserInfo.Id, flagModel.ServiceId, (FlagType)flagModel.Category, flagModel.Reason));
+
+            return Json(new { Success = true });
+        }
+    }
+}
---
+++ AtYourServiceWeb/Models/FlagModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AtYourService.Web.Models
+{
+    public class FlagModel
+    {
+        public byte Category { get; set; }
+
+        public string Reason { get; set; }
+
+        public int ServiceId { get; set; }
+    }
+}
